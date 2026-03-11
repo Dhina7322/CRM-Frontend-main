45903ef73d34c747ca0a6ed6e89bf22f1ac62c97
Dhina7322 <dhina7322@gmail.com>
2026-03-11 10:38:05 +0530

    Update register.jsx

--- src/auth/register.jsx
+++ src/auth/register.jsx
@@ -5,20 +5,20 @@ import bgvidieo from "../images/crm.mp4";
 import "../Styles/tailwind.css";
 export default function Register() {
   const navigate = useNavigate();
-const [, setLoading] = useState(false);
+  const [, setLoading] = useState(false);
   const [, setOtpSent] = useState(false);
-  
-const API = "https://crm-backend-347y.onrender.com";
+
+  const API = process.env.REACT_APP_API_URL;
 
   const [form, setForm] = useState({
     first_name: "",
     user_password: "",
-    email:"",
-    otp:"",
+    email: "",
+    otp: "",
     role: "user",
   });
 
- const sendOtp = async () => {
+  const sendOtp = async () => {
     if (!form.email) {
       alert("Enter email");
       return;
@@ -26,7 +26,7 @@ const API = "https://crm-backend-347y.onrender.com";
 
     try {
       setLoading(true);
-   await axios.post(`${API}/api/auth/send-email-otp`, { email: form.email });
+      await axios.post(`${API}/api/auth/send-email-otp`, { email: form.email });
       alert("OTP sent to email");
       setOtpSent(true);
     } catch (err) {
@@ -36,9 +36,9 @@ const API = "https://crm-backend-347y.onrender.com";
     }
   };
 
-//  Register
+  //  Register
 
-   const submit = async () => {
+  const submit = async () => {
     if (!form.otp) {
       alert("Please enter OTP");
       return;
@@ -60,7 +60,7 @@ const API = "https://crm-backend-347y.onrender.com";
 
   return (
     <div className="min-h-screen flex items-center justify-center background-register">
-       <video
+      <video
         autoPlay
         loop
         muted
@@ -75,7 +75,7 @@ const API = "https://crm-backend-347y.onrender.com";
         {/* LEFT FORM */}
         <div className="p-10 text-white bg-black/20 backdrop-blur-[0] w-[45%] mr-[120px] h-[100%] rounded-xl">
           <div className="flex items-center gap-2 ">
-            <div className="w-10 h-3"/>
+            <div className="w-10 h-3" />
             <span className="font-semibold text-yellow-300  relative right-[48px]">Madhura Softwares</span>
           </div>
 
@@ -99,41 +99,41 @@ const API = "https://crm-backend-347y.onrender.com";
                 setForm({ ...form, first_name: e.target.value })
               }
             />
-          <div className="flex gap-5 items-center ">
-            <input
-              placeholder="Enter Email"
-              type="email"
-              name="email"
-              className=" text-black px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 "
-              value={form.email}
-              onChange={(e) =>
-                setForm({ ...form, email: e.target.value })
-              }
-            />
-             <button onClick={sendOtp} className="py-3 bg-blue-500 text-white rounded-lg w-full">
+            <div className="flex gap-5 items-center ">
+              <input
+                placeholder="Enter Email"
+                type="email"
+                name="email"
+                className=" text-black px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 "
+                value={form.email}
+                onChange={(e) =>
+                  setForm({ ...form, email: e.target.value })
+                }
+              />
+              <button onClick={sendOtp} className="py-3 bg-blue-500 text-white rounded-lg w-full">
                 Get Otp
-             </button>
-             </div>
+              </button>
+            </div>
 
-             <div className="flex gap-4">
-             <input
-              placeholder="Enter Otp"
-              type="text"
-              name="otp"
-              className="w-full text-black px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 w-[300px]"
-              onChange={(e) =>
-                setForm({ ...form, otp: e.target.value })
-              }
-            />
+            <div className="flex gap-4">
+              <input
+                placeholder="Enter Otp"
+                type="text"
+                name="otp"
+                className="w-full text-black px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 w-[300px]"
+                onChange={(e) =>
+                  setForm({ ...form, otp: e.target.value })
+                }
+              />
 
-            <input
-              type="password"
-              placeholder="Password"
-              className="w-full px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 w-[300px]"
-              onChange={(e) =>
-                setForm({ ...form, user_password: e.target.value })
-              }
-            />
+              <input
+                type="password"
+                placeholder="Password"
+                className="w-full px-4 py-3 bg-white rounded-lg outline-none focus:ring-2 focus:ring-blue-500 w-[300px]"
+                onChange={(e) =>
+                  setForm({ ...form, user_password: e.target.value })
+                }
+              />
             </div>
             <select
               className="w-full px-4 py-3 bg-white text-black rounded-lg outline-none w-[300px]"
